3285350d58fea08345e0573e6caecf4ea4fb528b
XanderVi <engineermexanic@gmail.com>
2018-09-26 19:27:53 +0300

    add tests, hints, picture

--- verification/tests.py
+++ verification/tests.py
@@ -51,6 +51,46 @@ TESTS = {
                      ['x', 'x', 'x', 'f', 'h'],
                      ['z', 'z', 'z', 'z', 'g']],
             "answer": False
+        },
+        {
+            "input":[['b', 'a', 'E', 'r'],
+                     ['a', 'y', 'p', 'c'],
+                     ['x', 'H', 'B', 'U'],
+                     ['z', 'z', 'E', 'z']],
+            "answer": True
+        },
+        {
+            "input":[['h', 'y', 'p', 'e'],
+                     ['b', 'u', 'c', 'r'],
+                     ['h', 'e', 'r', 'b'],
+                     ['y', 'p', 'c', 'u']],
+            "answer": False
+        },
+        {
+            "input":[['h', 'h', 'h', 'y'],
+                     ['a', 'a', 'd', 'p'],
+                     ['b', 'e', 'x', 'e'],
+                     ['u', 'c', 'c', 'r']],
+            "answer": False
+        },
+        {
+            "input":[['U', 'C', 'r'],
+                     ['B', 'H', 'e'],
+                     ['e', 'Y', 'p']],
+            "answer": True
+        },
+        {
+            "input":[['h', 'y', 'p'],
+                     ['C', 'R', 'E'],
+                     ['u', 'b', 'e']],
+            "answer": True
+        },
+        {
+            "input":[['p', 'e', 'r'],
+                     ['y', 'y', 'e'],
+                     ['h', 'y', 'p']],
+            "answer": False
         }
+
     ]
 }
